3a7ba47767a3cc978bc0b670bca21f5af169fdd5
Herbstmeier <moritz.herbstmeier@gmx.de>
2022-11-18 03:10:26 +0100

    updated styling added tag sorting

--- Models/Project.cs
+++ Models/Project.cs
@@ -11,6 +11,15 @@ public class Project
         this.url = url;
     }
 
+    public Project(Project project)
+    {
+        this.title = project.title;
+        this.description = project.description;
+        this.imagePath = project.imagePath;
+        this.tags = project.tags;
+        this.url = project.url;
+    }
+
     public string title { get; set; }
     public string description { get; set; }
     public string imagePath { get; set; }
