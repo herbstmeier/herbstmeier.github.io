cb3a997aceeac9701cb3bd9fc9d0e21b1052f666
Herbstmeier <moritz.herbstmeier@gmx.de>
2023-01-11 17:53:38 +0100

    added bimeshof project skeleton added hover info box added github svg

--- Data/ProjectData.cs
+++ Data/ProjectData.cs
@@ -7,28 +7,44 @@ public static class ProjectData
         var projects = new List<Project>();
 
         projects.Add(
-        new Project(
-        "redpoint-title",
-        "redpoint-description",
-        "img/projects/redpoint.png",
-        new String[] { "Angular", "NodeJS", "MySQL", "Sass" },
-        "redpoint"));
+            new Project(
+                "redpoint",
+                "redpoint-description",
+                "img/projects/redpoint.png",
+                new String[] { "Angular", "NodeJS", "MySQL", "TypeScript", "HTML", "Sass" },
+                "redpoint"
+            )
+        );
 
         projects.Add(
-        new Project(
-        "portfolio-title",
-        "portfolio-description",
-        "img/projects/portfolio.png",
-        new String[] { "Blazor", "CSS" },
-        "portfolio"));
+            new Project(
+                "portfolio-title",
+                "portfolio-description",
+                "img/projects/portfolio.png",
+                new String[] { "Blazor", "C#", "HTML", "CSS" },
+                "portfolio"
+            )
+        );
 
         projects.Add(
-        new Project(
-        "alarm-title",
-        "alarm-description",
-        "img/projects/alarm.png",
-        new String[] { "Embedded", "Arduino", "C++", "Circuit Design" },
-        "alarm"));
+            new Project(
+                "alarm-title",
+                "alarm-description",
+                "img/projects/alarm.png",
+                new String[] { "Embedded", "Arduino", "C++", "Circuit Design" },
+                "alarm"
+            )
+        );
+
+        projects.Add(
+            new Project(
+                "bimeshof website",
+                "bimeshof-description",
+                "img/projects/bimeshof.png",
+                new String[] { "HTML", "CSS" },
+                "bimeshof"
+            )
+        );
 
         return projects;
     }
